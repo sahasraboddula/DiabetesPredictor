99d0d07db9450560f482c0d7e9f802c99d21f04a
Sahasra Boddula <sahasraboddula@gmail.com>
2025-02-23 16:18:20 -0800

    remove sns

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 from imblearn.under_sampling import RandomUnderSampler
 from collections import Counter
 from sklearn.preprocessing import LabelEncoder
